a62f13fceaa019556a8eb77aeb01c3ec268a2758
vinloren@gmail.com <vinloren@gmail.com>
2015-08-31 15:51:00 +0200

    Nuovo run_test_tcp.sh

---
+++ run_test_tcp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+node tcp_client.js 3334177443 8124 vinloren.ns0.it &
+node tcp_client.js 3319632138 8124 vinloren.ns0.it &
+node tcp_client.js 3349389533 8124 vinloren.ns0.it &
+node tcp_client.js 3334177442 8124 vinloren.ns0.it &
+node tcp_client.js 3319632139 8124 vinloren.ns0.it &
+node tcp_client.js 3349389534 8124 vinloren.ns0.it &
+node tcp_client.js 3334177444 8124 vinloren.ns0.it &
+node tcp_client.js 3319632140 8124 vinloren.ns0.it &
+node tcp_client.js 3349389535 8124 vinloren.ns0.it &
+node tcp_client.js 3334177445 8124 vinloren.ns0.it &
